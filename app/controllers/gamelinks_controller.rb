2a516d8a37ada80374d7552c6b2ccc5be5f427e8
S.H <gamelinks007@gmail.com>
2017-09-16 16:22:04 +0900

    問い合わせフォームの追加

--- app/controllers/gamelinks_controller.rb
+++ app/controllers/gamelinks_controller.rb
@@ -19,4 +19,7 @@ class GamelinksController < ApplicationController
 
   def link
   end
+
+  def contact
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'gamelinks007@gmail.com'
   layout 'mailer'
 end
