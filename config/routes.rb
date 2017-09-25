edfbf0db6910af86fcaeac40b2421787bf3ca5fd
S.H <gamelinks007@gmail.com>
2017-09-25 09:05:16 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   get 'gamelinks/game'
   get 'gamelinks/browsergame'
   get 'gamelinks/linksreference'
-  get 'gamelinks/blog'
   get 'gamelinks/link'
   get 'gamelinks/contact'
-  resources :articles do
-    resources :comments
   end
 end
