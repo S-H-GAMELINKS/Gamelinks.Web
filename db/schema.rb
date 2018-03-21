38d387bcdf3e5698b4317ff14de20ad73c86af5d
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-03-21 19:34:41 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916015637) do
+ActiveRecord::Schema.define(version: 20180321103333) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20170916015637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "toots", force: :cascade do |t|
+    t.string "toot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
