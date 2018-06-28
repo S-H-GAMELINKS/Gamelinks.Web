85e2937266836eb1043450218f2088024739f1b8
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-28 22:12:11 +0900

    fix slider

--- app/controllers/gamelinks_controller.rb
+++ app/controllers/gamelinks_controller.rb
@@ -1,5 +1,6 @@
 class GamelinksController < ApplicationController
   def home
+    @slide = ['links1.png', 'links2.png', 'hanakotoba1.png', 'hanakotoba2.png', 'igv1.png', 'igv2.png', 'bml1.png', 'bml2.png', 'moa1.png', 'moa2.png', 'vendetta1.png']
   end
 
   def about
