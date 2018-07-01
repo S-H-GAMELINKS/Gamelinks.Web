fbd3b4ff82c130d448a85cb6cbaf065a5adf271e
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-01 17:01:27 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180629042537) do
+ActiveRecord::Schema.define(version: 20180701075808) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "browsers", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
