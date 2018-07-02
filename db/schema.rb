da21527a1f7d333e704fe208b6ccf3ceee2d572f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-02 09:41:19 +0900

    add Game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701075808) do
+ActiveRecord::Schema.define(version: 20180702004012) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20180701075808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "images"
+    t.string "about"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", force: :cascade do |t|
