bf3e9becd1122ce5cdbe64313a2efdfea51a56ed
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-11 22:15:09 +0900

    add <br>

--- app/javascript/packs/components/member.vue
+++ app/javascript/packs/components/member.vue
@@ -3,10 +3,13 @@
         <h1>Member</h1>
         <h2 v-for="m in member">
             <p>名前:{{m.name}}</p>
+            <br>
             <p>主な担当</p>
             <p>{{m.position}}</p>
+            <br><br>
             <p>作品</p>
             <p>{{m.product}}</p>
+            <br><br>
         </h2>
         <br>
         <br>
