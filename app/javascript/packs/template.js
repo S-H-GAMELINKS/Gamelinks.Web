fd26af3c1e0602603adc77440adc6663ebd53b4d
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-11 13:25:54 +0900

    remove var set in template.js

--- app/javascript/packs/template.js
+++ app/javascript/packs/template.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import Template from './components/template.vue'
 
-//各ページのデータをセット
-var title    = page_title; 
-var about    = page_about;
-var message  = page_message;
-
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('template'))
   const template = new Vue({
